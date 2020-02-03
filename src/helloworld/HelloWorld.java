bd0e2e9a8bb26cb5360df3c830326f3f293da2ad
ekzhou <774653363@qq.com>
2020-02-03 20:41:07 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world!随便改一改");
+		System.out.println("hello world!闅忎究鏀规敼");
 	}
 }
